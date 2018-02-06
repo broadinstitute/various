0e93b417c586fed91ed7be2020d4b29d613da9c7
daltschu <daltschu@broadinstitute.org>
2018-02-06 16:27:24 -0500

    Bucket selection

--- ecs-migrate.sh
+++ ecs-migrate.sh
@@ -2,16 +2,53 @@
 
 threads=8
 log="/broad/stops/ecs-migration/logs/$dir.log"
-s3cmd="/git/archive-cli/s3cmd/s3cmd -c ~/git/archive-cli/.s3cfg_osarchive"
-boto="BOTO_CONFIG=/broad/stops/tools/ecs/.boto_ecs"
+s3cmd='/home/unix/daltschu/git/archive-cli/s3cmd/s3cmd -c /home/unix/daltschu/git/archive-cli/.s3cfg_osarchive'
+project='broad-archive-legacy'
 
-dir=
+
+bucket=
+
+
+#BULLSHIT I CANT GET WORKING
+#export BOTO_CONFIG=/broad/stops/ecs-migration/.boto_ecs
+#export BOTO_CONFIG=/home/unix/daltschu/.boto_ecs
+#boto='BOTO_CONFIG=/broad/stops/ecs-migration/.boto_ecs'
 
 eval export DK_ROOT="/broad/software/dotkit"; . /broad/software/dotkit/ksh/.dk_init 
-
 #use UGER
+#use .python-2.7.9-sqlite3-rtrees
 
-use .python-2.7.9-sqlite3-rtrees
+readarray -t dirs <<< "$( $s3cmd ls | cut -d ' ' -f 4)"
 
-echo "$boto /home/unix/daltschu/google-cloud-sdk/bin/gsutil -m rsync -r s3://irods-archive/attic/$dir/ gs://broad-ecs-irods-archive/attic/$dir/"
+echo -e "\nPlease select a bucket by number:\n"
+
+echo ""
+
+num=1
+
+for dir in "${dirs[@]:1}" ; do
+	echo "$num - $dir"
+	num=$((num+1))
+done
+
+echo ""
+
+read -p "Which bucket:" num_sel
+
+until [ "$num_sel" -le "${#dirs[@]}" ]
+do
+	if [ "$num_sel" -gt "${#dirs[@]}" ]; then
+		echo -e "not a valid bucket!\n"
+		read -p "Which bucket:" num_sel
+	fi
+done
+
+echo -e "You chose ${dirs[$num_sel]} \n"
+
+
+
+
+
+
+#echo "BOTO_CONFIG=/broad/stops/ecs-migration/.boto_ecs' /home/unix/daltschu/google-cloud-sdk/bin/gsutil -m rsync -r s3://irods-archive/attic/$dir/ gs://broad-ecs-irods-archive/attic/$dir/"
 
